15ccc2383c271c489538ce55185066c1c672f08c
Lory LETICEE <loryleticee@gmail.com>
2020-02-19 19:14:55 +0100

    change name an add env var

--- src/services/youtube/youtube.js
+++ src/services/youtube/youtube.js
@@ -1,5 +1,4 @@
-const TOKEN = "AIzaSyDt-Jo0h8x-uQ7EhMHR4GB7Ug2PY_3bt3w";
-const URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=ayrlo&type=video&key="+TOKEN;
+const URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=ayrlo&type=video&key="+process.env.REACT_APP_YOUTUBE_API_TOKEN;
 
 export const getVideos = () => {
     return fetch(URL)
