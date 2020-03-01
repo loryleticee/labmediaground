1a6f105372197cee9e5499fad843bc32670e9808
Lory LETICEE <loryleticee@gmail.com>
2020-03-01 23:41:40 +0100

    add margon top for youtube , change pge groupe in component

--- src/components/UI/Home/Home.js
+++ src/components/UI/Home/Home.js
@@ -1,83 +1,31 @@
 import React, {useEffect, useState} from "react";
 import "./Home.scss";
-import getVideos from "../../../services/youtube/youtube";
+import Youtube from "../../../services/youtube/youtube";
 
 const Home = () => {
-  const [videos, setVideos] = useState([]);
-  const init = [
-    {
-      id :
-        {
-          videoId: "Kxwwv6swXQk"
-        }
-    },
-    {
-      id :
-        {
-          videoId: "hoGY5RYvT0U"
-        }
-    },
-    {
-      id :
-        {
-          videoId: "xhKivXtrsGU"
-        }
-    },
-  ];
+  const [you, setYou] = useState([]);
 
-  const social = [
-    ""
-  ];
+  useEffect(() => {
+    if(you.length < 1){
+      setYou([1,2])
+    }
+  },);
 
-  useEffect(() =>{
-    let vdz = videos;
-    getVideos().then(data => {
-      if (vdz.length > 0 ) {
-      } else{
-        vdz = ['',''];
 
-        if(data['error'] !== undefined){
-          setVideos(
-            init
-          );
-          return;
-        }
-        setVideos(data.items);
-      }
-    });
-  },[videos]);
+  console.log('SUCCES',Youtube);
 
-    return (
-      <>
-
-        <div className="page-group">
-          <div className="social-group">
-            <iframe
-              width="78%" height="144"
-              scrolling="no" frameBorder="no"
-              allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/127696064&color=%23ffffff"
-            >
-            </iframe>
-
-          </div>
-          <div className={"youtube-canvas"}>
-            {
-              videos.map((video, index) => {
-               return (
-                 <iframe
-                   title={index}
-                   key ={index}
-                   src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                   frameBorder="0"
-                 />
-               )
-              })
-            }
-          </div>
-        </div>
-      </>
-    );
+    if(you.length > 0){
+      return (
+        <>
+          <Youtube/>
+        </>
+      );
+    }
+  return (
+    <>
+      <small>Wainting for a moment </small>
+    </>
+  );
 };
 
 export default Home;
--- src/services/youtube/youtube.js
+++ src/services/youtube/youtube.js
@@ -1,9 +1,92 @@
+import React, {useEffect, useState} from "react";
+
 const URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&order=date&q=ayrlo&type=video&key="+process.env.REACT_APP_YOUTUBE_API_TOKEN;
 
-export const getVideos = () => {
+ const getVideos = () => {
     return fetch(URL)
       .then(response => response.json())
       .catch(err => err || {});
 };
 
-export default getVideos;
+ const Youtube = (props) => {
+
+    const [videos, setVideos] = useState([]);
+    const init = [
+        {
+            id :
+              {
+                  videoId: "Kxwwv6swXQk"
+              }
+        },
+        {
+            id :
+              {
+                  videoId: "hoGY5RYvT0U"
+              }
+        },
+        {
+            id :
+              {
+                  videoId: "xhKivXtrsGU"
+              }
+        },
+    ];
+
+     useEffect(() =>{
+         let vdz = videos;
+         getVideos().then(data => {
+             if (vdz.length > 0 ) {
+             } else{
+                 vdz = ['',''];
+
+                 if(data['error'] !== undefined){
+                     setVideos(
+                       init
+                     );
+                     return;
+                 }
+                 setVideos(data.items);
+             }
+         });
+     },[videos]);
+
+    return (
+      <>
+        <div className="page-group">
+          <div className="social-group">
+              <iframe
+                width="78%" height="144"
+                scrolling="no" frameBorder="no"
+                allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/127696064&color=%23ffffff"
+              >
+              </iframe>
+              <iframe
+                width="78%" height="144"
+                scrolling="no" frameBorder="no"
+                allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/452966157&color=%23ffffff"
+              >
+              </iframe>
+
+          </div>
+          <div className={"youtube-canvas"}>
+              {
+                  videos.map((video, index) => {
+                      return (
+                        <iframe
+                          title={index}
+                          key ={index}
+                          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                          frameBorder="0"
+                        />
+                      )
+                  })
+              }
+          </div>
+      </div>
+    </>
+    );
+ };
+
+export default Youtube;
