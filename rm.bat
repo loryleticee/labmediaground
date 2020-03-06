33f56a9eb2f77b3c85c4a839b11ea7d8a09d48fa
LoryLeticeeP <loryleticee@gmail.com>
2020-03-06 02:53:27 -0500

    ...

--- rm.bat
+++ rm.bat
@@ -1,9 +1,9 @@
-rm -rf .idea favicon.ico static index.html manifest.json logo192.png logo512.png build asset-manifest.json service-worker.js precache-manifest.*.js &&
-yarn build &&
-cd build &&
-mv ./* ../ &&
-rm -rf build
-touch cron.txt
+rm -rf .idea favicon.ico static index.html manifest.json logo192.png logo512.png build asset-manifest.json service-worker.js precache-manifest.*.js ;
+#yarn build &&
+#cd build &&
+#mv ./* ../ &&
+#rm -rf build
+#touch cron.txt
 
 
 
