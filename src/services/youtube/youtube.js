4d33da387123d1a2042d22f3248bb0861cdae10d
Lory LETICEE <loryleticee@gmail.com>
2020-03-11 09:40:14 +0100

    ...

--- src/services/youtube/youtube.js
+++ src/services/youtube/youtube.js
@@ -46,7 +46,7 @@ const URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult
           </div>
           <div className={"youtube-canvas"}>
             <iframe
-              width="100%" height="450"
+              key="1" width="100%" height="450"
               scrolling="no" frameBorder="no" allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/4529664&color=%2364f7f7&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
             </iframe>
