1bb6cdc639468a41b8122eca796812602e0fcc0b
Lory LETICEE <loryleticee@gmail.com>
2020-04-15 22:09:42 +0200

    add social vk snap

--- src/services/youtube/youtube.js
+++ src/services/youtube/youtube.js
@@ -4,6 +4,7 @@ import insta from '../../images/instagram-icon.svg'
 import faceb from '../../images/facebook-icon.svg'
 import pinte from '../../images/pinterest-icon.svg'
 import twitt from '../../images/twitter-icon.svg'
+import vk from '../../images/vk-icon.svg'
 import vimeo from '../../images/vimeo-icon.svg'
 import youtu from '../../images/youtube-icon.svg'
 import snap from '../../images/snap-icon.jpg'
@@ -54,13 +55,14 @@ const Youtube = (props) => {
 
           <div className='robots-noindex robots-nofollow networks'>
             <img src={snap} alt='Ayrlo on Snapchat' />
-            <a href='https://ayrlomusic.tumblr.com' target='_top'><img src={tumb} alt='Ayrlo on Tumblr' /></a>
             <a href='https://www.twitch.tv/ayrlomusic' target='_top'><img src={twitch} alt='Ayrlo on Twitch' /></a>
+            <a href='https://twitter.com/ayrlo_music' target='_top'><img src={twitt} alt='Ayrlo on Twitter' /></a>
+            <a href='https://www.instagram.com/ayrlomusic_lory' target='_top'><img src={insta} alt='Ayrlo on Instagram' /></a>
+            <a href='https://vk.com/id588664621' target='_top'><img src={vk} alt='Donate to Ayrlo' /></a>
+            <a href='https://ayrlomusic.tumblr.com' target='_top'><img src={tumb} alt='Ayrlo on Tumblr' /></a>
             <a href='https://www.youtube.com/channel/UCazuAKGOsC6XXc-h1zRNWYw' target='_top'><img src={youtu} alt='Ayrlo on Youtube' /></a>
             <a href='https://vimeo.com/user110800363' target='_top'><img src={vimeo} alt='Ayrlo on Vimeo' /></a>
             <a href='https://www.pinterest.fr/Ayrlomusic' target='_top'><img src={pinte} alt='Ayrlo on Pinterest' /></a>
-            <a href='https://twitter.com/ayrlo_music' target='_top'><img src={twitt} alt='Ayrlo on Twitter' /></a>
-            <a href='https://www.instagram.com/ayrlomusic_lory' target='_top'><img src={insta} alt='Ayrlo on Instagram' /></a>
             <a href='https://www.facebook.com/AyrloOfficiel' target='_top'><img src={faceb} alt='Ayrlo on Facebook' /></a>
           </div>
 
