9287f62ceb9f5ef1546ad20aa1a3b32872af73e4
Lory LETICEE <loryleticee@gmail.com>
2020-08-13 02:46:33 +0200

    keep only spotify soundcloud and bandcamp

--- src/services/index/index.js
+++ src/services/index/index.js
@@ -1,9 +1,12 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 //import Social from '../social/social'
+import Soundcloud from '../soundcloud/soundcloud'
+import Bandcamp from '../bandcamp/bandcamp'
+import Spotify from '../spotify/spotify'
 
-const Soundcloud = React.lazy(() => import('../soundcloud/soundcloud'))
-const Bandcamp = React.lazy(() => import('../bandcamp/bandcamp'))
-const Spotify = React.lazy(() => import('../spotify/spotify'))
+//const Soundcloud = React.lazy(() => import('../soundcloud/soundcloud'))
+//const Bandcamp = React.lazy(() => import('../bandcamp/bandcamp'))
+//const Spotify = React.lazy(() => import('../spotify/spotify'))
 //const Tidal = React.lazy(() => import('../tidal/tidal'))
 
 const Index = (props) => {
@@ -12,14 +15,9 @@ const Index = (props) => {
       <div className='page-group'>
         <div className='social-group' />
         <div className='youtube-canvas'>
-          <Suspense fallback={
-            {/* */}
-          }
-          >
             <Soundcloud />
             <Bandcamp />
             <Spotify />
-          </Suspense>
         </div>
       </div>
     </>
