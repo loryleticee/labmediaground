1ca19f3b347f55b1ad18fbceafc108bfed8a533a
Lory LETICEE <loryleticee@gmail.com>
2020-08-31 22:56:15 +0200

    add merchandise link

---
+++ src/services/shop/shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import img from '../../images/shop.png'
+
+const Shop = (props) => {
+  return (
+    <div className='robots-noindex robots-nofollow'>
+      <img alt="Boutique Labmediaground"
+        key='soundcloud' title='AYRLOMUSIC Soundcloud playlist'
+        scrolling='no'
+        src={img}
+      />
+    </div>
+  )
+}
+
+export default Shop
