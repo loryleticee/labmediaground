bcd794d52ca338ccc2acffeb9c12b85902cb5eee
Lory LETICEE <loryleticee@gmail.com>
2020-08-31 23:34:52 +0200

    change img

--- src/services/shop/shop.js
+++ src/services/shop/shop.js
@@ -4,7 +4,7 @@ import img from '../../images/shop.jpg'
 const Shop = (props) => {
   return (
     <div className='robots-noindex robots-nofollow'>
-      <a href="https://www.ayrlomusic.com" ><img alt="Boutique Labmediaground"
+      <a href="https://www.labmediaground.com/product/sweat-a-capuche-ayrlomusic-logo-gris-ecriture-blanc/" ><img alt="Boutique Labmediaground"
         key='soundcloud' title='AYRLOMUSIC Soundcloud playlist'
         scrolling='no'
         src={img}
