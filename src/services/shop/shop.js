7a1bb71d8b12760188d50f70a6b43e0012864182
Lory LETICEE <loryleticee@gmail.com>
2020-09-01 04:55:01 +0200

    ...

--- src/services/shop/shop.js
+++ src/services/shop/shop.js
@@ -5,7 +5,7 @@ const Shop = () => {
   return (
     <div className='robots-noindex robots-nofollow' >
       <a href="https://www.labmediaground.com/product/sweat-a-capuche-ayrlomusic-logo-gris-ecriture-blanc/" ><img alt="Boutique Labmediaground"
-        key='soundcloud' title='AYRLOMUSIC Soundcloud playlist'
+        key='soundcloud' title='AYRLOMUSIC Merchandising'
         scrolling='no'
         src={img}
       /></a>
