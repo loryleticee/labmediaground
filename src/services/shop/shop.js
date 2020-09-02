ed4b3cf5ea8530cf09c3f7e5716a5cfa835f96fe
Lory LETICEE <loryleticee@gmail.com>
2020-09-03 00:01:22 +0200

    ..

--- src/services/shop/shop.js
+++ src/services/shop/shop.js
@@ -4,7 +4,7 @@ import img from '../../images/shop.jpg'
 const Shop = () => {
   return (
     <div className='robots-noindex robots-nofollow' >
-      <a href="https://www.labmediaground.com/product/sweat-a-capuche-ayrlomusic-logo-gris-ecriture-blanc/" ><img alt="Boutique Labmediaground"
+      <a href="https://www.labmediaground.com/product/sweat-a-capuche-ayrlomusic-logo-gris-ecriture-blanc/" target="_blank" rel="noopener noreferrer"><img alt="Boutique Labmediaground"
         key='soundcloud' title='AYRLOMUSIC Merchandising'
         scrolling='no'
         src={img}
