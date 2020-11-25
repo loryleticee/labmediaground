0da67fd885e2205e7aa05f3d4fa82072437767f7
Lory LÉTICÉE <loryleticee@gmail.com>
2020-11-25 22:08:19 +0100

    ..

--- src/services/shop/shop.js
+++ src/services/shop/shop.js
@@ -6,7 +6,7 @@ const Shop = () => {
       <a href='https://www.labmediaground.com/product/sac-a-dos/' target="_blank" rel="noopener noreferrer"><img alt="Boutique Labmediaground"
         key='soundcloud' title='AYRLOMUSIC Merchandising'
         scrolling='no'
-        src='https://www.labmediaground.com/wp-content/uploads/2020/11/mockup-e9b5b8b4.jpg'
+        src='https://www.labmediaground.com/wp-content/uploads/2020/11/mockup-0e7e2835-160x160.jpg'
       /></a>
     </div>
   )
